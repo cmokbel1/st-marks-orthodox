f821069970c5e9049723de9db73e94606eff6318
Claude Mokbel <claudemokbel@gmail.com>
2022-12-06 18:25:43 -0800

    create about page boiler plate

---
+++ src/Views/About/About.js
@@ -0,0 +1,17 @@
+export const About = () => {
+    return (
+        <>
+        <div className="hero-header">
+        <h1 className="page-title">About</h1>
+        </div>
+        <div className="container">
+           <p>Saint Mark Orthodox Christian Church is a parish of the Diocese of Los Angeles, and the West of the <a href="https://www.antiochian.org/home" target="_blank" rel="noreferrer">Antiochian Orthodox Christian Archdiocese of North America</a>.</p> 
+           <p>In 1994, the 200th anniversary of the Orthodox Christian presence in North America was celebrated. While Orthodoxy entered through Alaska with the Russian mission there in 1794, Orthodox people with roots in Greece, the Balkans, Africa, and the Middle East have since added to the diversity of our continent.</p>
+           <p>Immigrants from Syria and the Middle East began to organize Churches in 1895, and thereby the two thousand years of spiritual experience of the Antiochian Patriarchate has enriched the New World for more than one hundred years.</p>
+           <p>The Patriarchate of Antioch and All the East, together with the three other ancient Patriarchates (Constantinople, Alexandria, and Jerusalem); and the four modern Patriarchates (Russia, Serbia, Rumania, and Bulgaria); and the autocephalous Churches (of Cyprus, Greece, Georgia, Poland, and Albania); and the three autonomous Churches (Sinai, Czechoslovakia, and Finland) — with many dependent (daughter) bodies and missionary situations throughout the world — comprise what is known today as the “Eastern Orthodox Church” with an estimated 250 million adherents. All are united in faith and worship and share the Apostolic experience and tradition of the past twenty centuries.</p>
+           <p>...</p>
+           <p>The diversity of backgrounds at Saint Mark’s attests to the appeal of Orthodoxy at a time when many religious groups are viewed as accommodating and trendy. Many converts have sought out Orthodoxy in recent years, discovering its fidelity to scriptural and doctrinal foundations and its changeless expression of the historic Christian faith. If we can help you in your visit to St. Mark’s or your journey to Christ’s One, Holy, Catholic, and Apostolic Church, please <a href="#!">contact us</a>!</p>
+        </div>
+        </>
+    )
+}
